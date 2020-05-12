b2ed5daeae179a0d64ed92069cdfab0d9a74f456
Anthony Velez <velezanthony1@gmail.com>
2020-05-12 17:32:48 -0400

    Add functionality to mobile navigation

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,28 +1,55 @@
 const sidebar = document.querySelector('.sidebar');
 let sideWidth = sidebar.offsetWidth;
+let sideHeight = 0;
 const links = document.querySelectorAll('.nav-top__link');
+const navBtn = document.querySelector('.nav-top__btn');
+const mobile = window.matchMedia('(max-width: 1200px)');
 
-sidebar.addEventListener('click', (event) => {
-	if (sideWidth === 80) {
-		sidebar.classList.add('sidebar__active');
+if (mobile.matches) {
+	navBtn.addEventListener('click', (event) => {
+		if (sideHeight === 0) {
+			sidebar.classList.add('sidebar__mobile');
 
-		links.forEach((link) => {
-			link.style.visibility = 'visible';
-			link.style.opacity = '1';
-		});
+			links.forEach((link) => {
+				link.style.visibility = 'visible';
+				link.style.opacity = '1';
+			});
 
-		sideWidth = 250;
-	} else if (sideWidth === 250) {
-		sidebar.classList.remove('sidebar__active');
+			sideHeight = 120;
+		} else if (sideHeight === 120) {
+			sidebar.classList.remove('sidebar__mobile');
 
-		links.forEach((link) => {
-			link.style.visibility = 'hidden';
-			link.style.opacity = '0';
-		});
+			links.forEach((link) => {
+				link.style.visibility = 'hidden';
+				link.style.opacity = '0';
+			});
 
-		sideWidth = 80;
-	}
-});
+			sideHeight = 0;
+		}
+	});
+} else if (!mobile.matches) {
+	navBtn.addEventListener('click', (event) => {
+		if (sideWidth === 80) {
+			sidebar.classList.add('sidebar__active');
+
+			links.forEach((link) => {
+				link.style.visibility = 'visible';
+				link.style.opacity = '1';
+			});
+
+			sideWidth = 250;
+		} else if (sideWidth === 250) {
+			sidebar.classList.remove('sidebar__active');
+
+			links.forEach((link) => {
+				link.style.visibility = 'hidden';
+				link.style.opacity = '0';
+			});
+
+			sideWidth = 80;
+		}
+	});
+}
 
 //false buttons
 const falseBtns = document.querySelectorAll('#false');
